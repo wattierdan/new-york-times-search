4c3b95783e364f51222ad23f0cfbfd38a923e3a1
Dan <wattierdan@live.com>
2020-10-31 17:27:58 -0700

    adds function to script.js

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,39 @@
 //make Ajax call
 //dynamically make cards based on result
 
-//var searchbtn = $('#search')
-var searchTerm
-var numOfRecs
-var startYear
-var endYear
 
 
-$('#search').on('click', function(e){
+
+var resultContainer = $('#results-display')
+var key = "&api-key=Lt3FYwaGXu0TimyCtkZtH8LkZqaqYXfd"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+
+$('form').on('submit', function(e){
     e.preventDefault();
-    searchTerm = $('#search-term').val()
-    numOfRecs = $('#number-of-records').val()
-    startYear = $('#start-year').val()
-    endYear = $('#end-year').val()
+    $(".resultCard").remove()
+    $.ajax({
+        url: queryURL + $('#search-term').val() + key,
+        method: "get"
+      }).then(function(resp) {
+        var articles = resp.response.docs
+        //loop over docs array
+        var numOfRecs = $('#number-of-records').val()
+        for (var i =0; i < numOfRecs; i++){
+        //generate card for each item
+        console.log(articles[i].pub_year)
+           var resultDisplay = document.createElement('div')
+               resultDisplay.setAttribute('class', "resultCard")
+               resultDisplay.textContent = articles[i].headline.main + " " + articles[i].byline.original
+        //append results div
+               resultContainer.append(resultDisplay)
+        }
+    });
 
-
-    console.log(searchTerm, numOfRecs, startYear, endYear)
 })
+
+
+
+
+
+
+
